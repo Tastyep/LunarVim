4a54becaf18236194fc45cf8dc0cbb9790731054
Luc Sinet <luc.sinet@gmail.com>
2021-07-08 20:48:59 +0200

    Split plugin loading logic from the configuration

--- init.lua
+++ init.lua
@@ -2,13 +2,18 @@ require "default-config"
 require "keymappings"
 vim.cmd("luafile " .. CONFIG_PATH .. "/lv-config.lua")
 require "settings"
-require "plugins"
+
+local plugins = require "plugins"
+local plugin_loader = require("plugin-loader").init()
+plugin_loader:load { plugins, O.user_plugins }
 vim.g.colors_name = O.colorscheme -- Colorscheme must get called after plugins are loaded or it will break new installs.
+
 require "lv-utils"
 require "lv-treesitter"
 if O.plugin.dashboard.active then
   require("lv-dashboard").config()
 end
+
 -- TODO these gues need to be in language files
 -- require "lsp"
 -- if O.lang.emmet.active then
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,69 +1,43 @@
-local execute = vim.api.nvim_command
-local fn = vim.fn
-
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-
-if fn.empty(fn.glob(install_path)) > 0 then
-  execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
-  execute "packadd packer.nvim"
-end
-
-local packer_ok, packer = pcall(require, "packer")
-if not packer_ok then
-  return
-end
-
-packer.init {
-  git = { clone_timeout = 300 },
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "single" }
-    end,
-  },
-}
-
-return require("packer").startup(function(use)
+-- https://github.com/wbthomason/packer.nvim#specifying-plugins
+return {
   -- Packer can manage itself as an optional plugin
-  use "wbthomason/packer.nvim"
-
-  -- TODO refactor all of this (for now it works, but yes I know it could be wrapped in a simpler function)
-  use { "neovim/nvim-lspconfig" }
-  use { "kabouzeid/nvim-lspinstall", event = "VimEnter" }
+  { "wbthomason/packer.nvim" },
+  -- Quickstart LSP client configurations
+  { "neovim/nvim-lspconfig" },
+  -- LspInstall command
+  { "kabouzeid/nvim-lspinstall", event = "VimEnter" },
   -- Telescope
-  use { "nvim-lua/popup.nvim" }
-  use { "nvim-lua/plenary.nvim" }
-  use { "tjdevries/astronauta.nvim" }
-  use {
+  { "nvim-lua/popup.nvim" },
+  { "nvim-lua/plenary.nvim" },
+  { "tjdevries/astronauta.nvim" },
+  {
     "nvim-telescope/telescope.nvim",
     config = [[require('lv-telescope')]],
     --event = "BufEnter",
-  }
-
+  },
   -- Autocomplete
-  use {
+  {
     "hrsh7th/nvim-compe",
     event = "InsertEnter",
     config = function()
       require("lv-compe").config()
     end,
-  }
-
-  use { "hrsh7th/vim-vsnip", event = "InsertEnter" }
-  use { "rafamadriz/friendly-snippets", event = "InsertEnter" }
-
+  },
+  -- Snippets
+  { "hrsh7th/vim-vsnip", event = "InsertEnter" },
+  { "rafamadriz/friendly-snippets", event = "InsertEnter" },
   -- Treesitter
-  use { "nvim-treesitter/nvim-treesitter" }
-
-  -- Neoformat
-  use {
+  { "nvim-treesitter/nvim-treesitter" },
+  -- Formatter
+  {
     "sbdchd/neoformat",
     config = function()
       require "lv-neoformat"
     end,
     event = "BufRead",
-  }
-
-  use {
+  },
+  -- File explorer tree
+  {
     "kyazdani42/nvim-tree.lua",
     -- event = "BufEnter",
     -- cmd = "NvimTreeToggle",
@@ -71,38 +45,36 @@ return require("packer").startup(function(use)
     config = function()
       require("lv-nvimtree").config()
     end,
-  }
-
-  use {
+  },
+  -- Git signs
+  {
     "lewis6991/gitsigns.nvim",
 
     config = function()
       require("lv-gitsigns").config()
     end,
     event = "BufRead",
-  }
+  },
 
-  -- whichkey
-  use {
+  -- Keybindings popup
+  {
     "folke/which-key.nvim",
     config = function()
       require "lv-which-key"
     end,
     event = "BufWinEnter",
-  }
-
+  },
   -- Autopairs
-  use {
+  {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
     after = { "telescope.nvim" },
     config = function()
       require "lv-autopairs"
     end,
-  }
-
-  -- Comments
-  use {
+  },
+  -- Comment toggler
+  {
     "terrortylor/nvim-comment",
     event = "BufWinEnter",
     config = function()
@@ -112,35 +84,33 @@ return require("packer").startup(function(use)
       end
       nvim_comment.setup()
     end,
-  }
-
-  -- Color
-  use { "christianchiarulli/nvcode-color-schemes.vim", opt = true }
-
+  },
+  -- Color scheme
+  { "christianchiarulli/nvcode-color-schemes.vim", opt = true },
   -- Icons
-  use { "kyazdani42/nvim-web-devicons" }
-
-  -- Status Line and Bufferline
-  use {
+  { "kyazdani42/nvim-web-devicons" },
+  -- Status line
+  {
     "glepnir/galaxyline.nvim",
     config = function()
       require "lv-galaxyline"
     end,
     -- event = "VimEnter",
-  }
-
-  use {
+  },
+  -- Buffer line
+  {
     "romgrk/barbar.nvim",
     config = function()
       require "lv-barbar"
     end,
     event = "BufWinEnter",
-  }
+  },
 
-  -- Builtins, these do not load by default
+  -- [ Builtins ]
+  -- These do not load by default
 
   -- Dashboard
-  use {
+  {
     "ChristianChiarulli/dashboard-nvim",
     event = "BufWinEnter",
     -- cmd = { "Dashboard", "DashboardNewFile", "DashboardJumpMarks" },
@@ -149,9 +119,9 @@ return require("packer").startup(function(use)
     -- end,
     disable = not O.plugin.dashboard.active,
     -- opt = true,
-  }
-  -- Zen Mode
-  use {
+  },
+  -- Zen mode
+  {
     "folke/zen-mode.nvim",
     cmd = "ZenMode",
     -- event = "BufRead",
@@ -159,9 +129,9 @@ return require("packer").startup(function(use)
       require("lv-zen").config()
     end,
     disable = not O.plugin.zen.active,
-  }
-
-  use {
+  },
+  -- Color code colorizer
+  {
     "norcalli/nvim-colorizer.lua",
     event = "BufWinEnter",
     config = function()
@@ -169,16 +139,16 @@ return require("packer").startup(function(use)
       -- vim.cmd "ColorizerReloadAllBuffers"
     end,
     disable = not O.plugin.colorizer.active,
-  }
-
+  },
   -- Treesitter playground
-  use {
+
+  {
     "nvim-treesitter/playground",
     event = "BufRead",
     disable = not O.plugin.ts_playground.active,
-  }
-
-  use {
+  },
+  -- Indent guides
+  {
     "lukas-reineke/indent-blankline.nvim",
     event = "BufRead",
     setup = function()
@@ -196,113 +166,128 @@ return require("packer").startup(function(use)
       vim.g.indent_blankline_show_first_indent_level = true
     end,
     disable = not O.plugin.indent_line.active,
-  }
-
-  -- comments in context
-  use {
+  },
+  -- Comments in context
+  {
     "JoosepAlviste/nvim-ts-context-commentstring",
     event = "BufRead",
     disable = not O.plugin.ts_context_commentstring.active,
-  }
-
-  -- Symbol Outline
-  use {
+  },
+  -- Symbols tree view
+  {
     "simrat39/symbols-outline.nvim",
     cmd = "SymbolsOutline",
     disable = not O.plugin.symbol_outline.active,
-  }
-  -- diagnostics
-  use {
+  },
+  -- Diagnostics
+  {
     "folke/trouble.nvim",
     cmd = "TroubleToggle",
     disable = not O.plugin.trouble.active,
-  }
-
+  },
   -- Debugging
-  use {
+  {
     "mfussenegger/nvim-dap",
     config = function()
       require "lv-dap"
     end,
     disable = not O.plugin.debug.active,
-  }
-
+  },
   -- Floating terminal
-  use {
+  {
     "numToStr/FTerm.nvim",
     event = "BufRead",
     config = function()
       require("lv-floatterm").config()
     end,
     disable = not O.plugin.floatterm.active,
-  }
-
+  },
   -- Use fzy for telescope
-  use {
+  {
     "nvim-telescope/telescope-fzy-native.nvim",
     event = "BufRead",
     disable = not O.plugin.telescope_fzy.active,
-  }
-
+  },
   -- Use project for telescope
-  use {
+  {
     "nvim-telescope/telescope-project.nvim",
     event = "BufRead",
     setup = function()
       vim.cmd [[packadd telescope.nvim]]
     end,
     disable = not O.plugin.telescope_project.active,
-  }
-
+  },
   -- Sane gx for netrw_gx bug
-  use {
+  {
     "felipec/vim-sanegx",
     event = "BufRead",
     disable = not O.plugin.sanegx.active,
-  }
-
-  -- Diffview
-  use {
+  },
+  -- Git diff view
+  {
     "sindrets/diffview.nvim",
     event = "BufRead",
     disable = not O.plugin.diffview.active,
-  }
-
-  -- Lush Create Color Schemes
-  use {
+  },
+  -- Color scheme creation
+  {
     "rktjmp/lush.nvim",
     -- cmd = {"LushRunQuickstart", "LushRunTutorial", "Lushify"},
     disable = not O.plugin.lush.active,
-  }
-
+  },
   -- Debugger management
-  use {
+  {
     "Pocco81/DAPInstall.nvim",
     -- event = "BufRead",
     disable = not O.plugin.dap_install.active,
-  }
+  },
+  -- Pretty parentheses
+  {
+    "p00f/nvim-ts-rainbow",
+    disable = not O.plugin.ts_rainbow.active,
+  },
+  -- Autotags <div>|</div>
+  {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+    disable = not O.plugin.ts_autotag.active,
+  },
+  -- Custom semantic text objects
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    disable = not O.plugin.ts_textobjects.active,
+  },
+  -- Smart text objects
+  {
+    "RRethy/nvim-treesitter-textsubjects",
+    disable = not O.plugin.ts_textsubjects.active,
+  },
+  -- Text objects using hint labels
+  {
+    "mfussenegger/nvim-ts-hint-textobject",
+    event = "BufRead",
+    disable = not O.plugin.ts_hintobjects.active,
+  },
 
-  -- LANGUAGE SPECIFIC GOES HERE
-  use {
+  -- [ Language integrations ]
+
+  -- Latex
+  {
     "lervag/vimtex",
     ft = "tex",
     config = function()
       require "lv-vimtex"
     end,
-  }
-
-  -- Rust tools
-  -- TODO: use lazy loading maybe?
-  use {
+  },
+  -- Rust
+  {
     "simrat39/rust-tools.nvim",
     disable = not O.lang.rust.rust_tools.active,
-  }
-
+  },
   -- Elixir
-  use { "elixir-editors/vim-elixir", ft = { "elixir", "eelixir", "euphoria3" } }
-
+  { "elixir-editors/vim-elixir", ft = { "elixir", "eelixir", "euphoria3" } },
   -- Javascript / Typescript
-  use {
+  {
     "jose-elias-alvarez/nvim-lsp-ts-utils",
     ft = {
       "javascript",
@@ -312,13 +297,12 @@ return require("packer").startup(function(use)
       "typescriptreact",
       "typescript.tsx",
     },
-  }
-
-  use {
+  },
+  -- Java
+  {
     "mfussenegger/nvim-jdtls",
     disable = not O.lang.java.java_tools.active,
-  }
-
+  },
   -- use {
   --   "jose-elias-alvarez/null-ls.nvim",
   --   ft = {
@@ -333,40 +317,4 @@ return require("packer").startup(function(use)
   --     require("null-ls").setup()
   --   end,
   -- }
-
-  -- Pretty parentheses
-  use {
-    "p00f/nvim-ts-rainbow",
-    disable = not O.plugin.ts_rainbow.active,
-  }
-
-  -- Autotags <div>|</div>
-  use {
-    "windwp/nvim-ts-autotag",
-    event = "InsertEnter",
-    disable = not O.plugin.ts_autotag.active,
-  }
-
-  -- Custom semantic text objects
-  use {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    disable = not O.plugin.ts_textobjects.active,
-  }
-
-  -- Smart text objects
-  use {
-    "RRethy/nvim-treesitter-textsubjects",
-    disable = not O.plugin.ts_textsubjects.active,
-  }
-
-  -- Text objects using hint labels
-  use {
-    "mfussenegger/nvim-ts-hint-textobject",
-    event = "BufRead",
-    disable = not O.plugin.ts_hintobjects.active,
-  }
-
-  for _, plugin in pairs(O.user_plugins) do
-    packer.use(plugin)
-  end
-end)
+}
